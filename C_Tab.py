7d4d1560785d44f924c19c16ed5704984a065463
OpenSorcerer <91776042+L4Lovely@users.noreply.github.com>
2022-09-20 20:04:48 +0200

    Cursor verschwinden fixed
    > wenn der cursor im Stillstand ist und die Liste zu scrollen beginnt

--- C_Tab.py
+++ C_Tab.py
@@ -1,15 +1,18 @@
-from C_Render import C_Draw
+import os
 
 class C_Tab:
-    def __init__(self, cPos_relative,Path,offset,width,screen):
-        self.cPos_relative = cPos_relative
+    def __init__(self,Path,offset,width,screen,Render):
+        self.cPosPre = [offset+2,3]
+        self.cPos_origin = [offset+2,3]
+        self.cPos =  [offset+2,3]
+        self.cPos_relative = 0
         self.PathHistory = []
         self.PathHistory.append(Path)
         self.dirList    = []
         self.fileList   = []
         self.currDir    = []
 
-        self.Render     = C_Draw()
+        self.Render     = Render
 
         self.screen     = screen
         self.width      = width
@@ -39,10 +42,22 @@ class C_Tab:
 
         self.dirList.clear()
         self.fileList.clear()
-        self.Render._drawList(self._getPath(),dirList,fileList,currDir,listOrigin,screen)
+        self.Render._drawList(self._getPath(),dirList,fileList,currDir,listOrigin,screen,self.cPos_relative)
 
     def _drawTab(self):
         self.Render._drawTab(self.offset,self.width,self.screen)
 
     def _clearList(self):
-        self.Render._clear(self.offset,self.width,self.screen)
+        self.Render._clear(self.offset+1,self.width,self.screen)
+#das hier bis
+    def _cursorDel(self,screen):
+        screen.addstr(self.cPos[1],self.cPos[0],' ')
+   
+    def _cursorDraw(self,screen):
+        screen.addstr(self.cPos[1],self.cPos[0],'}')    
+#hier ist sch.... aber auf die schnelle nix anderes eingefallen(sollte in der dartunterliegenden klasse sein) -> FIXED :))
+    def _setCursor(self,screen):
+        if not self.cPos[1] == os.get_terminal_size().lines - 1: # verhindert das übermalen des eigentlichen cursors bei stillstand
+            screen.addstr(self.cPosPre[1],self.cPosPre[0],' ')
+        screen.addstr(self.cPos[1],self.cPos[0],'}')  
+
